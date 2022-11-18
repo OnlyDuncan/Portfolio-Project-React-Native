6028aaf4fff60f3062f1c1c58398b25530fd37e9
Duncan Payne <Duncan.C.Payne@gmail.com>
2022-11-18 14:05:40 -0600

    Stuff

--- Portfolio/screens/NewsScreen.js
+++ Portfolio/screens/NewsScreen.js
@@ -1,10 +1,19 @@
-import { Text, View } from 'react-native';
+import { Container, Text, View, } from 'react-native';
+import { Card } from 'react-native-elements';
 
 const NewsScreen = () => {
     <View>
-        <Text>
-            This is the news screen!
-        </Text>
+        <Card style={{ height: "100%" }}>
+            <CardTitle card-header border border-dark style={{ backgroundColor: 'rgb(117, 117, 117)' }}>
+                <Text text-light>Recently Produced Work:</Text>
+            </CardTitle>
+            <CardBody card-body border border-dark style={{ backgroundColor: 'rgb(244, 242, 242)' }}>
+                <Text>Title: "The Black Egg"</Text>
+                <Text>Medium: Mixed Media, Ashes on Watercolor Paper</Text>
+                <Text>Size: 22 inches x 16 inches</Text>
+                <Text>Year: 2022</Text>
+            </CardBody>
+        </Card>
     </View>
 };
 

fadfe410cbdebe2b7f1af42e52a447b17408b173
Duncan Payne <Duncan.C.Payne@gmail.com>
2022-11-19 14:16:13 -0600

    New Direction

--- Portfolio/screens/NewsScreen.js
+++ Portfolio/screens/NewsScreen.js
@@ -1,24 +1,10 @@
-import { Text, View } from 'react-native';
+import { Text, View, Image } from 'react-native';
 import { Card } from 'react-native-elements';
 import { ScrollView } from 'react-native-gesture-handler';
-import TheBlackEgg from '../assets/images/The-Black-Egg.jpeg';
 
 const NewsScreen = () => {
     return (
         <ScrollView>
-            <Card style={{ height: "100%" }}>
-                <Card.Title style={{ backgroundColor: 'rgb(117, 117, 117)' }}>
-                    Recently Produced Work:
-                </Card.Title>
-                <Card.Divider />
-                <Card.Image containerStyle={{ height: 300, flex: 1 }} source={TheBlackEgg} />
-                <View style={{ backgroundColor: 'rgb(244, 242, 242)' }}>
-                    <Text>Title: "The Black Egg"</Text>
-                    <Text>Medium: Mixed Media, Ashes on Watercolor Paper</Text>
-                    <Text>Size: 22 inches x 16 inches</Text>
-                    <Text>Year: 2022</Text>
-                </View>
-            </Card>
             <Card style={{ height: "100%" }}>
                 <Card.Title style={{ backgroundColor: 'rgb(117, 117, 117)' }}>
                     Current Shows and Exhibitions:

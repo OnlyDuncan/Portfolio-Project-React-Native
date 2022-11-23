7ec7ac58f5cb59fd771a3a2dd20ad96b569fea50
Matthew Coe <matthewcoe001@gmail.com>
2022-11-23 14:16:02 -0700

    Image Background

--- Portfolio/screens/EntryScreen.js
+++ Portfolio/screens/EntryScreen.js
@@ -1,11 +1,14 @@
-import { Text, View } from 'react-native';
+import { ImageBackground, Text, View } from 'react-native';
+import theBlackEgg from '../assets/images/The-Black-Egg.jpeg';
 
 const EntryScreen = () => {
     return (
         <View style={{ justifyContent: 'center', flex: 1, alignItems: 'center' }}>
-            <Text style={{ textAlign: 'center' }}>
-                This is the entry screen!
-            </Text>
+            <ImageBackground source={theBlackEgg} style={{ width: '100%', height: '100%' }}>
+                <Text style={{ textAlign: 'center', color: 'white' }}>
+                    This is the entry screen!
+                </Text>
+            </ImageBackground>
         </View>
     )
 };
